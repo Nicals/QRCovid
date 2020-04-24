d158de2424d778c13acf993aed8603edab79d6c3
Nicolas Appriou <nicolas.appriou@gmail.com>
2020-04-24 15:21:11 +0200

    betters reasons ticks

--- src/components/Certificate.vue
+++ src/components/Certificate.vue
@@ -31,37 +31,37 @@
     </p>
 
     <p>
-      <input type="checkbox" :checked="goingOut.reasons.includes('travail')">
+      <span class="cert-tick" :class="{checked: goingOut.reasons.includes('travail')}"></span>
       Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle, lorsqu'ils sont indispensables à l'exercice d’activités ne pouvant être organisées sous forme de télétravail ou déplacements professionnels ne pouvant être différés;
     </p>
 
     <p>
-      <input type="checkbox" :checked="goingOut.reasons.includes('courses')">
+      <span class="cert-tick" :class="{checked: goingOut.reasons.includes('courses')}"></span>
       Déplacements pour effectuer des achats de fournitures nécessaires à l’activité professionnelle et des achats de première nécessité dans des établissements dont les activités demeurent autorisées (liste sur gouvernement.fr);
     </p>
 
     <p>
-      <input type="checkbox" :checked="goingOut.reasons.includes('sante')">
+      <span class="cert-tick" :class="{checked: goingOut.reasons.includes('sante')}"></span>
       Consultations et soins ne pouvant être assurés à distance et ne pouvant être différés ; consultations et soins des patients atteints d'une affection de longue durée;
     </p>
 
     <p>
-      <input type="checkbox" :checked="goingOut.reasons.includes('famille')">
+      <span class="cert-tick" :class="{checked: goingOut.reasons.includes('famille')}"></span>
       Déplacements pour motif familial impérieux, pour l’assistance aux personnes vulnérables ou la garde d’enfants;
     </p>
 
     <p>
-      <input type="checkbox" :checked="goingOut.reasons.includes('sport')">
+      <span class="cert-tick" :class="{checked: goingOut.reasons.includes('sport')}"></span>
       éplacements brefs, dans la limite d'une heure quotidienne et dans un rayon maximal d'un kilomètre autour du domicile, liés soit à l'activité physique individuelle des personnes, à l'exclusion de toute pratique sportive collective et de toute proximité avec d'autres personnes, soit à la promenade avec les seules personnes regroupées dans un même domicile, soit aux besoins des animaux de compagnie.
     </p>
 
     <p>
-      <input type="checkbox" :checked="goingOut.reasons.includes('judiciaire')">
+      <span class="cert-tick" :class="{checked: goingOut.reasons.includes('judiciaire')}"></span>
       Convocation judiciaire ou administrative.
     </p>
 
     <p>
-      <input type="checkbox" :checked="goingOut.reasons.includes('missions')">
+      <span class="cert-tick" :class="{checked: goingOut.reasons.includes('missions')}"></span>
       Participation à des missions d’intérêt général sur demande de l’autorité administrative.
     </p>
 
