9a0b8c1a7e062f197922e8696831445e835077bb
Nicolas Appriou <nicolas.appriou@gmail.com>
2020-10-30 18:59:20 +0100

    change brand name

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
   <!-- brand -->
-  <router-link :to="{name: 'index'}" class="navbar-brand">QRCovid</router-link>
+  <router-link :to="{name: 'index'}" class="navbar-brand">QRCovid 2</router-link>
 
   <!-- hamburger -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-items" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
